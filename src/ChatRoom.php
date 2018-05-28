bb10bad2cc01911121741d4feaed7848a5502a1c
runphp <runphp@qq.com>
2018-05-28 21:45:14 +0800

    initial a new project

---
+++ src/ChatRoom.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace NimServer;
+
+
+class ChatRoom
+{
+
+}
