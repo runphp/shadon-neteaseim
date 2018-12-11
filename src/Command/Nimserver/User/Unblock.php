e7e68fe0f618937c3d567ac3c185f953e475f95b
runphp <runphp@qq.com>
2018-12-11 15:06:00 +0800

    Add Block and Unblock command

---
+++ src/Command/Nimserver/User/Unblock.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Shadon\Neteaseim\Command\Nimserver\User;
+
+use Psr\Http\Message\ResponseInterface;
+use Shadon\Neteaseim\Command\Action;
+
+/**
+ * 解禁网易云通信ID.
+ *
+ * @author hehui<runphp@dingtalk.com>
+ */
+class Unblock extends Action
+{
+    public function __construct(array $arguments)
+    {
+        $this->setUri('/nimserver/user/unblock.action');
+        parent::__construct($arguments);
+    }
+
+    public function __invoke(ResponseInterface $response)
+    {
+        $return = parent::__invoke($response);
+
+        return $return['code'];
+    }
+}
