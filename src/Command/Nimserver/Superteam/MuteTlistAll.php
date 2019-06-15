b2ceac7eca9ed9eaf89da35f0633aa331be30676
runphp <runphp@qq.com>
2019-06-16 02:08:22 +0800

    Add superteam

---
+++ src/Command/Nimserver/Superteam/MuteTlistAll.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Shadon\Neteaseim\Command\Nimserver\Superteam;
+
+use Psr\Http\Message\ResponseInterface;
+use Shadon\Neteaseim\Command\Action;
+
+/**
+ * 将群组整体禁言.
+ *
+ * @author hehui<runphp@dingtalk.com>
+ */
+class MuteTlistAll extends Action
+{
+    public function __construct(array $arguments)
+    {
+        $this->setUri('/nimserver/superteam/muteTlistAll.action');
+        parent::__construct($arguments);
+    }
+
+    public function __invoke(ResponseInterface $response)
+    {
+        $return = parent::__invoke($response);
+
+        return $return['code'];
+    }
+}
